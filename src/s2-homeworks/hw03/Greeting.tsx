25eb457b6f1c04019d4bfd6dad83fc7e67ad41d0
mvl3x <125395126+mvl3x@users.noreply.github.com>
2023-06-15 14:39:04 +0500

    fix: inputClass

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -25,7 +25,7 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = s.errorInput // need to fix with (?:)
+    const inputClass = error && s.errorInput // need to fix with (?:)
 
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
@@ -42,7 +42,7 @@ const Greeting: React.FC<GreetingPropsType> = (
                         id={'hw3-input'}
                         value={name}
                         onChange={setNameCallback}
-                        className={inputClass}
+                        className={[inputClass, s.input].join(' ')}
                         onKeyDown={onEnter}
                         onBlur={onBlur}
                     />
