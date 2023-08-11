748a89be4d1c15fb150ad1ffdef5c27ceff86126
mvl3x <125395126+mvl3x@users.noreply.github.com>
2023-08-11 17:50:58 +0500

    fix: fix homeWorkReducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,23 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { 
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            return [...state].sort((current, next) => {
+                    if (current.name > next.name){
+                        return action.payload === "up" ? 1 : -1;
+                    } else if (current.name < next.name) {
+                        return action.payload === "up" ? -1 : 1;
+                    } else {
+                        return 0;
+                    }
+                })
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user => user.age > action.payload)
         }
         default:
-            return state
+            return state;
     }
 }
