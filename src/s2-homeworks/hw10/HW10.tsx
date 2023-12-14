88f53f4de495315503b2f782418a0e815880d23a
mvl3x <125395126+mvl3x@users.noreply.github.com>
2023-12-14 18:04:12 +0500

    fix: get isLoading and dispatch from redux

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -15,7 +15,8 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector<AppStoreType, boolean>((state) => state.loading.isLoading)
+    const dispatch = useDispatch();
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
