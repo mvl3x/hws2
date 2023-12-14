438357e7371fac51ea816748e267955793b41d7d
mvl3x <125395126+mvl3x@users.noreply.github.com>
2023-12-14 18:03:11 +0500

    feat: add logic for loadingReducer

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -7,7 +7,8 @@ type TLoadingState = {
 export const loadingReducer = (state = initState, action: LoadingActionType): TLoadingState => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-
+        case 'CHANGE_LOADING':
+            return {...state, isLoading: action.isLoading}
         default:
             return state
     }
